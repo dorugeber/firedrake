2db717170dbf6b638ea7a5602416b52a049d0df9
Reuben Hill <ReubenHill@users.noreply.github.com>
2020-04-08 15:13:40 +0100

    add missing import in VertexOnlyMesh

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -1830,6 +1830,7 @@ def VertexOnlyMesh(mesh, vertexcoords, comm=COMM_WORLD):
     """
 
     import firedrake.functionspace as functionspace
+    import firedrake.function as function
 
     mesh.init()
 
